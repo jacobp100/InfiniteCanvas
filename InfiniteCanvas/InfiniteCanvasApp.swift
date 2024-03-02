df42ea39f44423cc86f82cbc811a450debb4e9ed
Jacob Parker <jacobparker1992@gmail.com>
2024-03-02 11:25:22 +0000

    Initial Commit

---
+++ InfiniteCanvas/InfiniteCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteCanvasApp.swift
+//  InfiniteCanvas
+//
+//  Created by Jacob Parker on 02/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
